98f2b3be424b678f7ccf52f7f0cd60e92303e150
luisrocha <luis.rocha910@gmail.com>
2024-03-25 19:45:38 -0500

    added to app and created datajs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import puppyList from './data.js';
+import {useState} from "react";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+  function handleClick() {
+    // some logic here
+  }
 
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <div className="App">
+      <p>{ featPupId && <p>{ featPupId }</p> } </p>
+      
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => {setFeatPupId(puppy.id)}}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
